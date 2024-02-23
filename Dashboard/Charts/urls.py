615380fd7e5c7d3697de8ccd9777d05c880f68ab
DavidST <d.strucken@mat-solutions.de>
2024-02-23 17:11:15 +0100

    ergniewerte

--- Dashboard/Charts/urls.py
+++ Dashboard/Charts/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('Charts/getPerformanceMeasureFourHour/', views.getPerformanceMeasureFourHour, name='getPerformanceMeasureFourHour'),
 
     path('energiewerte/', views.energiewerte, name='energiewerte'),
+    path('energiewerte/get_energy_consumption_one_hour_update', views.get_energy_consumption_one_hour_update, name='get_energy_consumption_one_hour_update'),
     path('energiewerte/get_energy_consumption_one_hour', views.get_energy_consumption_one_hour, name='get_energy_consumption_one_hour'),
+    path('energiewerte/get_energy_consumption_four_hour', views.get_energy_consumption_four_hour, name='get_energy_consumption_four_hour'),
 
     path('umweltwerte/', views.umweltwerte, name='umweltwerte'),
     path('umweltwerte/get_temperatur_one_hour', views.get_temperatur_one_hour, name='get_temperatur_one_hour'),
--- Dashboard/Charts/views.py
+++ Dashboard/Charts/views.py
@@ -114,11 +114,11 @@ def energiewerte(request):
 
     # Query for the data
     query_Actual_Values = """from(bucket: "AdaNowoTest")
-    |> range(start: -34h, stop: now())
-    |> filter(fn: (r) => r["_measurement"] == "SetValues" and r["_field"] == "PerformanceMeasure")
-    |> group(columns: ["_field"])
-    |> sort(columns: ["_time"], desc: false)
-    """
+        |> range(start: -1h, stop: now())
+        |> filter(fn: (r) => r["_measurement"] == "QualityValues")
+        |> filter(fn: (r) => r["_field"] == "Energy")
+        |> aggregateWindow(every: 1m, fn: last)
+        |> yield(name: "last")"""
 
     # Execute the Flux query and store the result in tables
     tables = query_api.query(query_Actual_Values, org="MAT")
@@ -127,9 +127,12 @@ def energiewerte(request):
 
 
     for table in tables:
+        print(table)
         for record in table.records:
             value = record.values["_value"]
             time = record.values["_time"]
+            if value == None: 
+                value = 0
             energy_consumption.append(value)
             formatted_datetime = time.strftime("%H:%M:%S")
             energy_consumption_time.append(formatted_datetime)
@@ -142,20 +145,48 @@ def energiewerte(request):
     return render(request, 'Charts/energiewerte.html', energy_consumption_context)
 
 
-# Function for updating the "Energieverbrauch"- Chart
-def get_energy_consumption_one_hour(request):
-    energy_consumption = []
+
+# Function for updating the "Energieverbrauch"- Charts with 1min interval
+def get_energy_consumption_one_hour_update(request):
 
     client_influxdb = InfluxDBClient(url="http://localhost:8086", token="Qc6s7RKI7ZnQpB5ZdesJzEmgd46XLGRmcXv5RJRbhTUc758Ma8g-LQv6_A2p125BZohkhbYnEhVtpeOHJ-BqTw==", org="MAT")
 
     query_api = client_influxdb.query_api()
 
     query_Actual_Values = """from(bucket: "AdaNowoTest")
-    |> range(start: 0, stop: now())
-    |> filter(fn: (r) => r["_measurement"] == "SetValues" and r["_field"] == "PerformanceMeasure")
-    |> group(columns: ["_field"])
-    |> sort(columns: ["_time"], desc: true)
-    |> limit(n: 1)"""
+        |> range(start: 0, stop: now())
+        |> filter(fn: (r) => r["_measurement"] == "QualityValues" and r["_field"] == "Energy")
+        |> group(columns: ["_field"])
+        |> sort(columns: ["_time"], desc: true)
+        |> limit(n: 1)"""
+
+    tables = query_api.query(query_Actual_Values, org="MAT")
+
+
+    for table in tables:
+        for record in table.records:
+            value = record.values["_value"]
+      
+    print(value)
+    return JsonResponse(value, safe=False)
+
+
+
+# Function for slecting time-selector "1h"
+def get_energy_consumption_one_hour(request):
+    energy_consumption_one = []
+    energy_consumption_one_time = []
+
+    client_influxdb = InfluxDBClient(url="http://localhost:8086", token="Qc6s7RKI7ZnQpB5ZdesJzEmgd46XLGRmcXv5RJRbhTUc758Ma8g-LQv6_A2p125BZohkhbYnEhVtpeOHJ-BqTw==", org="MAT")
+
+    query_api = client_influxdb.query_api()
+
+    query_Actual_Values = """from(bucket: "AdaNowoTest")
+        |> range(start: -1h, stop: now())
+        |> filter(fn: (r) => r["_measurement"] == "QualityValues")
+        |> filter(fn: (r) => r["_field"] == "Energy")
+        |> aggregateWindow(every: 1m, fn: last)
+        |> yield(name: "last")"""
 
     tables = query_api.query(query_Actual_Values, org="MAT")
 
@@ -164,10 +195,56 @@ def get_energy_consumption_one_hour(request):
         for record in table.records:
             value = record.values["_value"]
             time = record.values["_time"]
-            energy_consumption.append(value)
+            if value == None: 
+                value = 0
+            energy_consumption_one.append(value)
+            formatted_datetime = time.strftime("%H:%M:%S")
+            energy_consumption_one_time.append(formatted_datetime)
+
+    print(energy_consumption_one)
+    print(energy_consumption_one_time)
+
+    energy_consumption_context = {'energy_consumption_one': energy_consumption_one, 'energy_consumption_one_time': energy_consumption_one_time}
+
+    return JsonResponse(energy_consumption_context, safe=False)
 
 
-    return JsonResponse(value, safe=False)
+
+# Function for slecting time-selector "4h"
+def get_energy_consumption_four_hour(request):
+    energy_consumption_four = []
+    energy_consumption_four_time = []
+
+    client_influxdb = InfluxDBClient(url="http://localhost:8086", token="Qc6s7RKI7ZnQpB5ZdesJzEmgd46XLGRmcXv5RJRbhTUc758Ma8g-LQv6_A2p125BZohkhbYnEhVtpeOHJ-BqTw==", org="MAT")
+
+    query_api = client_influxdb.query_api()
+
+    query_Actual_Values = """from(bucket: "AdaNowoTest")
+        |> range(start: -4h, stop: now())
+        |> filter(fn: (r) => r["_measurement"] == "QualityValues")
+        |> filter(fn: (r) => r["_field"] == "Energy")
+        |> aggregateWindow(every: 1m, fn: last)
+        |> yield(name: "last")"""
+
+    tables = query_api.query(query_Actual_Values, org="MAT")
+
+
+    for table in tables:
+        for record in table.records:
+            value = record.values["_value"]
+            time = record.values["_time"]
+            if value == None: 
+                value = 0
+            energy_consumption_four.append(value)
+            formatted_datetime = time.strftime("%H:%M:%S")
+            energy_consumption_four_time.append(formatted_datetime)
+
+    print(energy_consumption_four)
+    print(energy_consumption_four_time)
+
+    energy_consumption_context = {'energy_consumption_four': energy_consumption_four, 'energy_consumption_four_time': energy_consumption_four_time}
+
+    return JsonResponse(energy_consumption_context, safe=False)
 
 
 #######################################
