4ee8a443e103a1bd10bb8071238b6052f98738df
DavidST <d.strucken@mat-solutions.de>
2024-03-14 11:28:07 +0100

    changed settings

--- Dashboard/Dashboard/settings.py
+++ Dashboard/Dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h48+dy7%^@6%31j1au)3lr_41n*pt2&&#66^(o!qn_^i*t$vzq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
